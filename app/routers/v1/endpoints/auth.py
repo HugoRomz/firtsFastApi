81895f0113d0135fafe31fbf4281791d1e508c65
HugoRomz <rosalesrafael1@hotmail.com>
2024-12-20 02:13:10 -0600

    Se actualizó el modelo de respuesta del endpoint "/me" para utilizar UserOut en lugar de RegisterOut

--- app/routers/v1/endpoints/auth.py
+++ app/routers/v1/endpoints/auth.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, Query
 from app.schemas.roles import RoleOut
 from app.schemas.auth import RegisterSchema, LoginSchema, TokenResponseSchema, RegisterOut
+from app.schemas.user import UserOut
 from app.services.auth import register_user, login_user, logout_user, get_public_roles
 from app.dependencies.auth import get_current_user
 
@@ -21,8 +22,8 @@ async def logout(user: TokenResponseSchema = Depends(get_current_user)):
     return await logout_user(user)
 
 
-@router.get("/me", response_model=RegisterOut)
-async def get_me(current_user: RegisterOut = Depends(get_current_user)):
+@router.get("/me", response_model=UserOut)
+async def get_me(current_user: UserOut = Depends(get_current_user)):
     return current_user
 
 @router.get("/roles", response_model=List[RoleOut])
